fb17a9dc0f7ac0b38c52f24448561b48a73bd322
Tobias Schneck <tobias.schneck@loodse.com>
2019-07-23 21:09:39 +0200

    comment encryption config

--- 50-encryption.sh
+++ 50-encryption.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### see https://kubernetes.io/docs/tasks/administer-cluster/encrypt-data/#understanding-the-encryption-at-rest-configuration
+
 ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 cat > encryption-config.yaml <<EOF
 kind: EncryptionConfig
